0e213539d2b189f7afbb4605091083aa959d3c69
Andrew Liikala <Liikalaandrew97@gmail.com>
2015-02-04 08:37:38 -0800

    Scraping Project; Restarting
    Decided to scrap what I've got and use the ideas I came up with/tricks I
    learned to restart fresh and take in a different route!

--- Boa.py
+++ Boa.py
@@ -1,21 +1,9 @@
 class boa(object):
-    def __init__(self, string, tag):#Assuming that this markup language is like xml or html
-        self.f = []#First occurences of tag
-        self.l = []#Last occurences of tag
-        self.ot = tag                            #Opening Tag
-        self.otl = len(tag)                      #Length of Opening Tag
-        self.otp = tag[0:self.otl-1] + ' '       #Incase the opening tag has parameters
-        self.ct = tag[0] + '/' + tag[1:self.otl] #Closign Tag
+    def __init__(self, string):#Assuming that this markup language is like xml or html
+        #self.f = []#First occurences of tag
+        #self.l = []#Last occurences of tag
+        #Plan To Include Another Module Version With MultiThreading
         self.data = string                       #Object storage of the string
-        x = string.count(self.ot)
-        y = string.count(self.otp)
-        if x == -1:
-            x = 0
-        elif y == -1:
-            y = 0
-        self.oto = x + y                         #How many times the opening tag appears
-        self.cto = string.count(self.ct)         #How many times the closing tag appears
-        self.instances = []                      #Array of dictionaries, where each dictionary is the data collected from each instance of tag
 
     def parameterAnalysis(self,x,y,z):           #To deal with opening tags containing parameters
         d = {}
@@ -35,45 +23,68 @@ class boa(object):
         else:
             d['parameters'] = 'none'
         d['content'] = z
-        self.instances.append(d)
+        self.instances.append(d)                #Possible just return d when called, so parameterAnalysis doesn't need to include self. Does not including slef make it faster?
 
-    def findMarkups(self):
-        if self.oto != self.cto: #If there are an uneven distribution of opening and closing tags
+    def readMarkups(self,ot,otl,otp,ct,oto,cto):
+        self.instances = []      #Array of dictionaries, where each dictionary is the data collected from each instance of tag
+        if oto != cto:           #If there are an uneven distribution of opening and closing tags
             print "There are an uneven number of opening and closing tags in this string!"
-            print self.oto
-            print self.cto
+            print oto
+            print cto
         else:
             fci = 0
             z = self.data
-            while fci != self.oto:
+            while fci != oto:
                 h = len(z)
-                a = z.find(self.ot)
-                b = z.find(self.otp)
+                a = z.find(ot)
+                b = z.find(otp)
                 #The if-statement determines whether or not the the plain tag or tag with parameters is first
                 if a>b and b != -1:
-                    v = z[b+self.otl:h]
+                    v = z[b+otl:h]
                     l = v.find('>')
                     v = v[0:l]                #Tag Parameters In $$$="%%%" Format, Spererated By Spaces
-                    f = len(v) + self.otl + 2
+                    f = len(v) + otl + 2
                     q = b
                 else:
                     q = a
                     v = 'none'
-                    f = self.otl + 1
-                c = z.find(self.ct)
+                    f = otl + 1
+                c = z.find(ct)
                 r = z[q+f-1:c]                #Contains the content between the tags
-                z = z[c+self.otl+1:h]         #Shortens string by removing the closing tag analyzed and all data before it!
+                z = z[c+otl+1:h]              #Shortens string by removing the closing tag analyzed and all data before it!
                 self.parameterAnalysis(v,0,r) #Sends all the different Parameter Tags and There Values To Be Indexed   ~ 1 being a temporary input, 1 will be replaced for an instance beign counted.
                 fci = fci + 1
+
+    def scrapeByTag(self, tag):
+        ot = tag                            #Opening Tag
+        otl = len(tag)                      #Length of Opening Tag
+        otp = tag[0:otl-1] + ' '            #Incase the opening tag has parameters
+        ct = tag[0] + '/' + tag[1:otl]      #Closign Tag
+        string = self.data
+        x = string.count(ot)
+        y = string.count(otp)
+        if x == -1:
+            x = 0
+        elif y == -1:
+            y = 0
+        oto = x + y                         #How many times the opening tag appears
+        cto = string.count(ct)              #How many times the closing tag appears
+        self.readMarkups(ot, otl, otp, ct, oto, cto)
+    #uid:'663554', username:'d4768a91-e3e7-4fd7-be27-7f89687068d7',
+    def renderDictionary(self, l):
+        a = l.split(',')
+        b = len(a) - 1
+        rD = {}
+        while b != -1:
+            c = a[b].split(":'")
+            d = c[0].strip()
+            e = c[1].replace("'", "")
+            rD[d] = e
+            b = b - 1
+        return rD
+    #Need A Function that converts unicode utf-8 to ASCII
+    
                 
-data = '''<strong>Courses</strong><ul><li><a target="_top" href="/section/default.asp?id=201540%2D44584"><span id="butter" class='toast'>SPRING 2015 INTERNET/INTRANET DES & DEV (WDD121-007-44584 Yaeger)</span></a></li><li><a target="_top" href="/section/default.asp?id=201540%2D43381"><span>SPRING 2015 POLITICAL SCIENCE (PSC121-006-43381 Selegean-Dostal)</span></a></li><li><a target="_top" href="/section/default.asp?id=201540%2D43834"><span>SPRING 2015 PRECALCULUS (MTH135-007-43834 Terakedis)</span></a></li><li><a target="_top" href="/section/default.asp?id=201540%2D40553"><span>SPRING 2015 SCIENCE/ENERGY AND THE ENV (BIO126-006-40553 Kane-Sutton)</span></a></li><li><a target="_top" href="/section/default.asp?id=201540%2D40031"><span>SPRING 2015 TECHNICAL REPORT WRITING (ENG221-001-40031 O'Brien)</span></a></li></ul>'''
-x = boa(data, '<span>')
-x.findMarkups()
-y = x.instances
-z = len(y)
-print "There are " + str(z) + " instaces of the tag!"
-print y[0]['content']
-print y[1]['content']
-print y[2]['content']
-print y[3]['content']
-print y[4]['content']
+
+
+
